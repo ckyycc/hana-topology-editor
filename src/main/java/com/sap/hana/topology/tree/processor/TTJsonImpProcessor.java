c8e6b504438b2add188d31b13ec84d8542703b27
CK <chengkuang@gmail.com>
2019-10-24 20:52:39 -0700

    Fixed the bug that "long" number will be parsed to double.

--- src/main/java/com/sap/hana/topology/tree/processor/TTJsonImpProcessor.java
+++ src/main/java/com/sap/hana/topology/tree/processor/TTJsonImpProcessor.java
@@ -1,10 +1,17 @@
 package com.sap.hana.topology.tree.processor;
 
-import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonSyntaxException;
+import com.google.gson.internal.LinkedTreeMap;
 import com.google.gson.reflect.TypeToken;
 import com.sap.hana.topology.tree.TTNode;
 import com.sap.hana.topology.util.FileUtils;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 /** Processor for importing name server JSON topology file */
@@ -21,6 +28,7 @@ public final class TTJsonImpProcessor implements TTProcessor<String, TTNode<Stri
    * @return topology tree root node
    */
   @Override
+  @SuppressWarnings("unchecked")
   public TTNode<String> process(String topologyStr) throws TTProcessException {
     // this is the root name in topology json file,
     // Should not use TopologyTreeUtils.TOPOLOGY_TREE_ROOT_NAME, because this just happens to be the
@@ -29,8 +37,16 @@ public final class TTJsonImpProcessor implements TTProcessor<String, TTNode<Stri
 
     Map<String, Object> topologyMap;
     try {
+      Type tokenType = new TypeToken<Map<String, Object>>() {}.getType();
       topologyMap =
-          new Gson().fromJson(topologyStr, new TypeToken<Map<String, Object>>() {}.getType());
+          new GsonBuilder()
+              .registerTypeAdapter(
+                  tokenType,
+                  (JsonDeserializer<Map<String, Object>>) // customizing the read logic
+                      (json, typeOfT, context) -> (Map<String, Object>) read(json))
+              .create()
+              .fromJson(topologyStr, tokenType);
+
     } catch (JsonSyntaxException e) {
       throw new TTProcessException("File format is not supported, JSON parse error!");
     }
@@ -39,8 +55,38 @@ public final class TTJsonImpProcessor implements TTProcessor<String, TTNode<Stri
       throw new TTProcessException("File format is not supported!");
     }
 
-    TTNode<String> root = new TTNode<>(ROOT_STRING);
-    return buildTopologyTree(topologyMap.get(ROOT_STRING), root);
+    return buildTopologyTree(topologyMap.get(ROOT_STRING), new TTNode<>(ROOT_STRING));
+  }
+
+  /**
+   * All the "long" numbers will be parsed as double, which is wrong for this topology tree, because
+   * the topology tree node does not have any Doubles. This function overwrites the original read
+   * logic, all string, number and boolean will be treated as string
+   *
+   * @param in JsonElement
+   * @return parsed object, should be list, map or string
+   */
+  private Object read(JsonElement in) {
+    if (in.isJsonArray()) {
+      // logic for array
+      List<Object> list = new ArrayList<>();
+      for (JsonElement element : in.getAsJsonArray()) {
+        list.add(read(element));
+      }
+      return list;
+    } else if (in.isJsonObject()) {
+      // logic for map
+      Map<String, Object> map = new LinkedTreeMap<>();
+      for (Map.Entry<String, JsonElement> entry : in.getAsJsonObject().entrySet()) {
+        map.put(entry.getKey(), read(entry.getValue()));
+      }
+      return map;
+    } else if (in.isJsonPrimitive()) {
+      // logic for primitive type, including string, number and boolean.
+      // The string, number, boolean will all be treated as string.
+      return in.getAsJsonPrimitive().getAsString();
+    }
+    return null;
   }
 
   /**
@@ -57,14 +103,10 @@ public final class TTJsonImpProcessor implements TTProcessor<String, TTNode<Stri
       for (String key : mapItem.keySet()) {
         node.addChild(buildTopologyTree(mapItem.get(key), new TTNode<>(node, key)));
       }
+    } else if (obj instanceof String) {
+      node.setValue((String) obj);
     } else {
-      if (obj instanceof Double) {
-        // Gson uses "double" for those "long" numbers, change it bak because we do not have
-        // "double" in JSON file
-        node.setValue(String.valueOf(((Double) obj).longValue()));
-      } else {
-        node.setValue(String.valueOf(obj));
-      }
+      node.setValue(String.valueOf(obj));
     }
     return node;
   }
